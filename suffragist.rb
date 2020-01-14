70f7e481b0ab767b2c8f16711dc79a1e96773385
Rathersilly <rathersilly@live.com>
2020-01-13 21:23:51 -0600

    tutorial finished

--- suffragist.rb
+++ suffragist.rb
@@ -1,5 +1,6 @@
 # https://guides.railsgirls.com/sinatra-app
 require 'sinatra'
+require 'yaml/store'
 
 Choices = {
   'HAM' => 'Hamburger',
@@ -16,7 +17,21 @@ end
 post '/cast' do
   @title = 'Thanks for casting your vote!'
   @vote = params['vote']
+  @store = YAML::Store.new 'votes.yml'
+  @store.transaction do
+    @store['votes'] ||= {}
+    @store['votes'][@vote] ||= 0
+    @store['votes'][@vote] += 1
+  end
   erb :cast
 end
+
+get '/results' do
+  @title = 'Results so far:'
+  @store = YAML::Store.new 'votes.yml'
+  @votes = @store.transaction { @store['votes'] }
+  erb :results
+end
+
 
 
